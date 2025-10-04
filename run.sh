02f0ccc7fd183e95f78508b55de8e7d0371ad16e
rlavelle <rowan.lavelle@gmail.com>
2025-10-04 13:51:01 +0800

    echo spend size

--- run.sh
+++ run.sh
@@ -4,6 +4,8 @@ set -euo pipefail
 funds=$(wc -l < data/funds.txt)
 spend=$(wc -l < data/spend.txt)
 
+echo "$spend"
+
 if [ "$funds" -eq "$spend" ]; then
 	gcc -o bayesian_main ccore/*.c ccore/distributions/*.c ccore/samplers/*.c ccore/simulation/*.c -lm
 	./bayesian_main	
